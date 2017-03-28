02774fcaa38a2e2bdb485d709fe54772c1e996fa
Yurii Myronchuk <bladeroot@gmail.com>
2017-03-28 08:51:07 +0000

    robokassa init

---
+++ src/Message/CompletePurchaseRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * RoboKassa driver for the Omnipay PHP payment processing library
+ *
+ * @link      https://github.com/hiqdev/omnipay-epayservice
+ * @package   omnipay-epayservice
+ * @license   MIT
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace Omnipay\RoboKassa\Message;
+
+/**
+ * RoboKassa Complete Purchase Request.
+ */
+class CompletePurchaseRequest extends AbstractRequest
+{
+    /**
+     * Get the data for this request.
+     *
+     * @return array request data
+     */
+    public function getData()
+    {
+        $this->validate('purse');
+
+        return $this->httpRequest->request->all();
+    }
+
+    /**
+     * Send the request with specified data.
+     *
+     * @param mixed $data The data to send
+     *
+     * @return CompletePurchaseResponse
+     */
+    public function sendData($data)
+    {
+        return $this->response = new CompletePurchaseResponse($this, $data);
+    }
+}
---
+++ src/Message/PurchaseRequest.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * RoboKassa driver for the Omnipay PHP payment processing library
+ *
+ * @link      https://github.com/hiqdev/omnipay-epayservice
+ * @package   omnipay-epayservice
+ * @license   MIT
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace Omnipay\RoboKassa\Message;
+
+class PurchaseRequest extends AbstractRequest
+{
+    public function getCurrency()
+    {
+        return $this->getParameter('currency');
+    }
+
+    public function setCurrency($value)
+    {
+        return $this->setParameter('currency', $value);
+    }
+
+    public function getLanguage()
+    {
+        return $this->getParameter('language');
+    }
+
+    public function setLanguage($value)
+    {
+        return $this->setParameter('language', $value);
+    }
+
+    public function getSignatureValue()
+    {
+        return $this->getParameter('signature');
+    }
+
+    public function setSignatureValue($value)
+    {
+        return $this->setParameter('signature', $value);
+    }
+
+    public function getData()
+    {
+        $this->validate(
+            'purse',
+            'amount', 'currency', 'description',
+            'returnUrl', 'cancelUrl', 'notifyUrl'
+        );
+
+        return [
+            'Desc' => $this->getDescription(),
+            'MrchLogin' => $this->getPurse(),
+            'OutSum' => $this->getAmount(),
+            'IncCurrLabel' => $this->getCurrency(),
+            'Culture' => $this->getLanguage(),
+            'ShpCart' => $this->is_cart,
+            'ShpClient' => $this->client,
+            'ShpTime' => $this->time,
+            'SignatureValue' => $this->getSignatureValue(),
+        ];
+    }
+
+    public function sendData($data)
+    {
+        return $this->response = new PurchaseResponse($this, $data);
+    }
+}
