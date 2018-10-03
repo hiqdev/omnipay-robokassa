5b400b72d6b969eda28384cfe54c22ad9ebc4658
WGQI <31042511+WGQI@users.noreply.github.com>
2018-10-03 16:33:31 +0300

    Update PurchaseResponse.php

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -37,7 +37,7 @@ class PurchaseResponse extends AbstractResponse implements RedirectResponseInter
 
     public function getRedirectMethod()
     {
-        return 'POST';
+        return 'GET';
     }
 
     public function getRedirectData()
