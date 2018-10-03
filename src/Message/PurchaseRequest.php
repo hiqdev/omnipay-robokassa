3ed8865e8204cdd0bb060e4df2db2eb01b0f2d09
WGQI <31042511+WGQI@users.noreply.github.com>
2018-10-03 16:34:21 +0300

    Update PurchaseRequest.php

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -19,7 +19,7 @@ class PurchaseRequest extends AbstractRequest
         );
 
         return [
-            'InvId' => 0,
+            'InvId' => $this->getInvId(),
             'MrchLogin' => $this->getPurse(),
             'OutSum' => $this->getAmount(),
             'Desc' => $this->getDescription(),
@@ -34,7 +34,7 @@ class PurchaseRequest extends AbstractRequest
         $params = [
             $this->getPurse(),
             $this->getAmount(),
-            0,
+            $this->getInvId(),
             $this->getSecretKey()
         ];
 
