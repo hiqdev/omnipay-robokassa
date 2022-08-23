9eefde0da52c52df0c72ae84d407bfcb38fa74ed
Yurii Myronchuk <bladeroot@gmail.com>
2022-08-23 08:54:53 +0300

    HP-919: fix warnings

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -89,7 +89,7 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
      */
     public function getReceipt()
     {
-        return rawurlencode($this->getParameter('receipt'));
+        return rawurlencode($this->getParameter('receipt') ?? '');
     }
 
     /**
